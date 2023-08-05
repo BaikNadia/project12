5f81aea405040536d13dee28b2734a07fa7928e7
baiko <baikowa.nadia@gmail.ru>
2023-08-05 11:24:02 +0300

    fixed2

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,6 +4,8 @@ import pytest
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
+
 
 def test_get_index():
     with pytest.raises(IndexError):
@@ -13,3 +15,6 @@ def test_get_index():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -1) == [3]
+    assert arrs.my_slice([1, 2, 3], -4) == [1, 2, 3]
+    assert arrs.my_slice([], -4) == []
